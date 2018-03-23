3c175a7e989d20c12be80dc4e266a0a34fa2f5a1
Pavel Borin <borin.pavel@phystech.edun>
2018-03-23 19:12:38 +0300

    detele .out and update

--- first_steps/hello_world.cpp
+++ first_steps/hello_world.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <thread>
 
+class thread_guard {
+public:
+    explicit thread_guard(std::thread t) :
+        t_(std::move(t))
+    {}
+
+    ~thread_guard()
+    {
+        if (t_.joinable()) {
+            t_.join();
+        }
+    }
+
+    thread_guard(const thread_guard& guard) = delete;
+    thread_guard& operator=(const thread_guard& guard) = delete;
+
+private:
+    std::thread t_;
+};
+
+
 void hello()
 {
     std::cout << "Hello, Concurrent World!\n";
@@ -8,7 +29,19 @@ void hello()
 
 int main()
 {
+
     std::thread t(hello);
-    t.join();
+    std::thread t2([]() {
+        std::cout << "Hello from lambda!\n";
+    });
+    try {
+        throw 0;
+    } catch (...) {
+        t.join();
+        t2.join();
+    }
+    thread_guard guard(std::thread{hello});
+
     return 0;
 }
+
